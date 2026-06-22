7d55149b75f64ad07df0182ffaf2ccab802ba37e
Andrew Pace <andr.pace@outlook.com>
2026-06-22 09:30:39 -0500

    adding nav bar to chart page

--- common/nav.js
+++ common/nav.js
@@ -1,13 +1,18 @@
 function loadNav() {
+    const isSubPage = window.location.pathname.includes("/tools/");
+
+    const homePath = isSubPage ? "../../index.html" : "index.html";
+    const chartPath = isSubPage ? "index.html" : "tools/chart-app/index.html";
+
     const nav = document.createElement("div");
 
     nav.innerHTML = `
         <div id="navbar">
-            <a href="/tool-suite/index.html">Home</a>
-            <a href="/tool-suite/tools/chart-app/index.html">Chart Tool</a>
-            <a href="/tool-suite/tools/time-study/index.html">Time Study</a>
+            <a href="${homePath}">Home</a>
+            <a href="${chartPath}">Chart Tool</a>
         </div>
     `;
 
     document.body.insertBefore(nav, document.body.firstChild);
 }
+
